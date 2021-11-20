9b984b30b290df6e7a2be7c1cd9dce0bc8225509
Miodrag Tomic <mtappdevelopment@outlook.com>
2021-11-20 14:57:03 +0100

    search icon, input field pseudo class states

--- src/components/SearchableIcon/index.jsx
+++ src/components/SearchableIcon/index.jsx
@@ -1,15 +1,16 @@
 import styles from './SearchableIcon.module.css';
 import { SpinnerIcon } from './SpinnerIcon';
+import { SearchIcon } from './SeachIcon';
 
 export function SearchableIcon(props){
-  const loading = true;
+  const loading = false;
 
   return (
   <div className={styles['searchable-icon']}>
     {
       loading 
         ? (<SpinnerIcon />)
-        : (<SearchableIcon />)
+        : (<SearchIcon />)
     }
   </div>)
 }
